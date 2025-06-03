84002d5712dbb9e1087ba7762fbd4737f99a7deb
Ilan Kirschenbaum <ilan@practical-agile.com>
2025-06-04 00:00:14 +0300

    Add an online API on vercel.com

---
+++ api/render.py
@@ -0,0 +1,23 @@
+from plasticinefont.renderer import generate_text_image
+from io import BytesIO
+from PIL import Image
+import os
+
+def handler(request, response):
+    text = request.args.get("text", "WAY TO GO")
+    letter_folder = request.args.get("letter_folder", "data/AlphaCaps")
+    spacing = int(request.args.get("spacing", 10))
+
+    output = BytesIO()
+    generate_text_image(
+        text=text,
+        output_path=output,
+        letter_folder=letter_folder,
+        spacing=spacing
+    )
+
+    output.seek(0)
+    response.status_code = 200
+    response.headers["Content-Type"] = "image/png"
+    response.body = output.read()
+    return response

ffdd4a6711ee41cff88dedb85df16f7c84af180b
Ilan Kirschenbaum <ilan@practical-agile.com>
2025-06-04 02:32:04 +0300

    Change vercel.json api/render includeFiles to String

--- api/ping.py
+++ api/ping.py
@@ -2,5 +2,4 @@ def handler(request, response):
     response.status_code = 200
     response.body = "pong"
     response.headers["Content-Type"] = "application/json"
-    response.body = '{"status": "ok"}'
     return response

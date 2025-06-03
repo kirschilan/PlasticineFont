020c0b63e99c74460d4a9496d1f2827bf8681c36
Ilan Kirschenbaum <ilan@practical-agile.com>
2025-06-04 02:44:08 +0300

    Simplify ping.py and render.py

--- api/ping.py
+++ api/ping.py
@@ -1,5 +1,4 @@
 def handler(request, response):
     response.status_code = 200
     response.body = "pong"
-    response.headers["Content-Type"] = "application/json"
     return response
--- api/render.py
+++ api/render.py
@@ -1,20 +1,14 @@
-from plasticinefont import config
 from plasticinefont.renderer import generate_text_image
 from io import BytesIO
-from PIL import Image
-import os
 
 def handler(request, response):
     text = request.args.get("text", "WAY TO GO")
-    letter_folder = request.args.get("letter_folder", config.DEFAULT_LETTER_FOLDER)
     spacing = int(request.args.get("spacing", 10))
 
     output = BytesIO()
     generate_text_image(
         text=text,
-        output_path=None,  # Explicitly prevent accidental use
         output_stream=output,
-        letter_folder=letter_folder,
         spacing=spacing
     )
 

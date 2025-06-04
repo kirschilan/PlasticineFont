6b4deadb0759d11796b0aecd411ff0030e4fa409
Ilan Kirschenbaum <ilan@practical-agile.com>
2025-06-04 23:58:26 +0300

    Implement redner.py as inherits from BaseHTTPRequestHandler

--- api/render.py
+++ api/render.py
@@ -1,19 +1,27 @@
+from http.server import BaseHTTPRequestHandler
+from urllib.parse import parse_qs, urlparse
 from plasticinefont.renderer import generate_text_image
 from io import BytesIO
 
-def handler(request, response):
-    text = request.args.get("text", "WAY TO GO")
-    spacing = int(request.args.get("spacing", 10))
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Parse query parameters
+        query = parse_qs(urlparse(self.path).query)
+        text = query.get("text", ["WAY TO GO"])[0]
+        spacing = int(query.get("spacing", [10])[0])
 
-    output = BytesIO()
-    generate_text_image(
-        text=text,
-        output_stream=output,
-        spacing=spacing
-    )
+        # Generate image
+        output = BytesIO()
+        generate_text_image(
+            text=text,
+            output_stream=output,
+            spacing=spacing
+        )
+        output.seek(0)
+        image_bytes = output.read()
 
-    output.seek(0)
-    response.status_code = 200
-    response.headers["Content-Type"] = "image/png"
-    response.body = output.read()
-    return response
+        # Send response
+        self.send_response(200)
+        self.send_header('Content-type','image/png')
+        self.end_headers()
+        self.wfile.write(image_bytes)
